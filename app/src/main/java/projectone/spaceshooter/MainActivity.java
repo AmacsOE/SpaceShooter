8b5068c4d38b0b40110b1c9a084e4eefa81c5c56
Ács Imre <37476463+OEandroid2018@users.noreply.github.com>
2018-04-23 06:20:14 +0200

    űrhajó forgatása

--- app/src/main/java/projectone/spaceshooter/MainActivity.java
+++ app/src/main/java/projectone/spaceshooter/MainActivity.java
@@ -1,13 +1,26 @@
 package projectone.spaceshooter;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
+    ImageView urhajo;
+    Button jobbraButton;
+    Button balraButton;
+
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +29,43 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+
+
+        urhajo = findViewById(R.id.urhajo);
+        jobbraButton = findViewById(R.id.jobbra_fordul);
+        balraButton = findViewById(R.id.balra_fordul);
+
+        final Animation jobbraAnimation = new AnimationUtils().loadAnimation(MainActivity.this, R.anim.fordul_jobbra);
+        final Animation balraAnimation = new AnimationUtils().loadAnimation(MainActivity.this, R.anim.fordul_balra);
+
+
+        jobbraButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    urhajo.startAnimation(jobbraAnimation);
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    urhajo.clearAnimation();
+                }
+                return true;
+            }
+        });
+
+        balraButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    urhajo.startAnimation(balraAnimation);
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    urhajo.clearAnimation();
+                }
+                return true;
+            }
+        });
+
+
+
     }
 }
