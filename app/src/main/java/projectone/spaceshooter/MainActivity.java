ee426d29e65c676aa80c5501e78206b34f3cebb7
Ács Imre <37476463+OEandroid2018@users.noreply.github.com>
2018-05-12 21:57:33 +0200

    lehet a hajóval fordulni folyamatosan

--- app/src/main/java/projectone/spaceshooter/MainActivity.java
+++ app/src/main/java/projectone/spaceshooter/MainActivity.java
@@ -2,11 +2,12 @@ package projectone.spaceshooter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends Activity {
@@ -14,6 +15,11 @@ public class MainActivity extends Activity {
     ViewFlipper urhajo;
     Button jobbraButton;
     Button balraButton;
+
+    private boolean jobbraFolyamatosan = false;
+    private boolean balraFolyamatosan = false;
+    private final long REPEAT_DELAY = 10;
+    private Handler repeatUpdateHandler = new Handler();
 
 
     @Override
@@ -34,27 +40,93 @@ public class MainActivity extends Activity {
         jobbraButton = findViewById(R.id.jobbra_fordul);
         balraButton = findViewById(R.id.balra_fordul);
 
-        //fordulás gombnyomásra
+        class RepetitiveUpdater implements Runnable {
+
+            @Override
+            public void run() {
+                if (jobbraFolyamatosan) {
+                    jobbraFordul();
+                    repeatUpdateHandler.postDelayed(new RepetitiveUpdater(), REPEAT_DELAY);
+                } else if (balraFolyamatosan) {
+                    balraFordul();
+                    repeatUpdateHandler.postDelayed(new RepetitiveUpdater(), REPEAT_DELAY);
+                }
+            }
+
+        }
+
         jobbraButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                int rot;
-                rot = (int) urhajo.getRotation();
-                rot += 1;
-                urhajo.setRotation(rot);
+            public void onClick(View v) {
+                jobbraFordul();
+            }
+        });
+
+        jobbraButton.setOnLongClickListener(new View.OnLongClickListener() {
+
+            @Override
+            public boolean onLongClick(View v) {
+                jobbraFolyamatosan = true;
+                repeatUpdateHandler.post(new RepetitiveUpdater());
+                return false;
+            }
+        });
+
+        jobbraButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP && jobbraFolyamatosan) {
+                    jobbraFolyamatosan = false;
+                }
+                return false;
             }
         });
 
         balraButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                int rot;
-                rot = (int) urhajo.getRotation();
-                rot -= 1;
-                urhajo.setRotation(rot);
+            public void onClick(View v) {
+                balraFordul();
             }
         });
 
-}
+        balraButton.setOnLongClickListener(new View.OnLongClickListener() {
+
+            @Override
+            public boolean onLongClick(View v) {
+                balraFolyamatosan = true;
+                repeatUpdateHandler.post(new RepetitiveUpdater());
+                return false;
+            }
+        });
+
+        balraButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP && balraFolyamatosan) {
+                    balraFolyamatosan = false;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    public void jobbraFordul() {
+        int rot;
+        rot = (int) urhajo.getRotation();
+        if (rot == 359){
+            rot = 0;
+        }else rot += 1;
+        urhajo.setRotation(rot);
+        }
+
+    public void balraFordul() {
+        int rot;
+        rot = (int) urhajo.getRotation();
+        if (rot == 0){
+            rot = 359;
+        }else rot -= 1;
+        urhajo.setRotation(rot);
+    }
 
 }
