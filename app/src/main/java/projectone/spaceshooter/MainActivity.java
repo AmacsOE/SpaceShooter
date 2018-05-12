363372ee562ffec91fd7221108367756ebef7c2c
Ács Imre <37476463+OEandroid2018@users.noreply.github.com>
2018-05-12 21:23:10 +0200

    alap felület + a hajó felszállt, minimálisan kanyarodik is

--- app/src/main/java/projectone/spaceshooter/MainActivity.java
+++ app/src/main/java/projectone/spaceshooter/MainActivity.java
@@ -1,30 +1,26 @@
 package projectone.spaceshooter;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends Activity {
 
-    ImageView urhajo;
+    ViewFlipper urhajo;
     Button jobbraButton;
     Button balraButton;
 
 
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //teljes képernyős mód
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
@@ -32,40 +28,33 @@ public class MainActivity extends Activity {
 
 
         urhajo = findViewById(R.id.urhajo);
+        urhajo.startFlipping();
+
+
         jobbraButton = findViewById(R.id.jobbra_fordul);
         balraButton = findViewById(R.id.balra_fordul);
 
-        final Animation jobbraAnimation = new AnimationUtils().loadAnimation(MainActivity.this, R.anim.fordul_jobbra);
-        final Animation balraAnimation = new AnimationUtils().loadAnimation(MainActivity.this, R.anim.fordul_balra);
-
-
-        jobbraButton.setOnTouchListener(new View.OnTouchListener() {
+        //fordulás gombnyomásra
+        jobbraButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
-                    urhajo.startAnimation(jobbraAnimation);
-                }
-                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    urhajo.clearAnimation();
-                }
-                return true;
+            public void onClick(View view) {
+                int rot;
+                rot = (int) urhajo.getRotation();
+                rot += 1;
+                urhajo.setRotation(rot);
             }
         });
 
-        balraButton.setOnTouchListener(new View.OnTouchListener() {
+        balraButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
-                    urhajo.startAnimation(balraAnimation);
-                }
-                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    urhajo.clearAnimation();
-                }
-                return true;
+            public void onClick(View view) {
+                int rot;
+                rot = (int) urhajo.getRotation();
+                rot -= 1;
+                urhajo.setRotation(rot);
             }
         });
 
+}
 
-
-    }
 }
